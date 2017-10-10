c96c45492400c402af041095de871de579cb0e2d
jack-a-scott <jack.scott@ntlworld.com>
2017-10-10 11:34:21 +0100

    set up resource routes

--- showme_project/db/seeds.rb
+++ showme_project/db/seeds.rb
@@ -6,6 +6,12 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-Article.create!(title: "Article 1", description: "Desc 1", bg_image: "url", proj_image: "p_url", git_link: "gitlink", difficulty: 4, user_id: 1)
-Article.create!(title: "Article 2", description: "Desc 2", bg_image: "url", proj_image: "p_url", git_link: "gitlink", difficulty: 4, user_id: 1)
-Article.create!(title: "Article 3", description: "Desc 3", bg_image: "url", proj_image: "p_url", git_link: "gitlink", difficulty: 4, user_id: 1)
+
+u1 = User.create! :email => 'q@q.com', :password => 'qqqqqq', :password_confirmation => 'qqqqqq'
+u2 = User.create! :email => 'w@w.com', :password => 'wwwwww', :password_confirmation => 'wwwwww'
+
+
+
+Article.create!(title: "Article 1", description: "Desc 1", bg_image: "url", proj_image: "p_url", git_link: "gitlink", difficulty: 4, user_id: u1.id)
+Article.create!(title: "Article 2", description: "Desc 2", bg_image: "url", proj_image: "p_url", git_link: "gitlink", difficulty: 4, user_id: u1.id)
+Article.create!(title: "Article 3", description: "Desc 3", bg_image: "url", proj_image: "p_url", git_link: "gitlink", difficulty: 4, user_id: u2.id)
