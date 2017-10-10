dae018c463c6f0e4a204dfffb7266ff00b3f932d
GNorfolk <georgenorfolk@hotmail.co.uk>
2017-10-10 12:29:00 +0100

    Further validation added to models/article.rb.

--- showme_project/app/models/article.rb
+++ showme_project/app/models/article.rb
@@ -3,6 +3,14 @@ class Article < ApplicationRecord
 
 	validates :title, :description, :bg_image, :proj_image, :git_link, :difficulty, :user_id presence: true
 
+	validates :title , length: { minimum: 2, maximum: 256, message: "must be between 2 and 256 characters long!" }
+
+	validates :description , length: { minimum: 2, maximum: 4096, message: "must be between 2 and 4096 characters long!" }
+
+	validates :git_link, format: { with: URI.regexp, message: "must be a valid URL link!" }
+
+	validates :difficulty, numericality: { greater_than: 0, less_than: 6, message: "must be from 1 to 5!" }
+
     # t.string "title"
     # t.text "description"
     # t.string "bg_image"
