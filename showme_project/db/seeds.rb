30ff86a3baff97b6fa57f2dd850bb943f6718e26
jack-a-scott <jack.scott@ntlworld.com>
2017-10-10 11:12:22 +0100

    Created some seed data

--- showme_project/db/seeds.rb
+++ showme_project/db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Article.create!(title: "Article 1", description: "Desc 1", bg_image: "url", proj_image: "p_url", git_link: "gitlink", difficulty: 4, user_id: 1)
+Article.create!(title: "Article 2", description: "Desc 2", bg_image: "url", proj_image: "p_url", git_link: "gitlink", difficulty: 4, user_id: 1)
+Article.create!(title: "Article 3", description: "Desc 3", bg_image: "url", proj_image: "p_url", git_link: "gitlink", difficulty: 4, user_id: 1)
