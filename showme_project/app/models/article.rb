dc966e8afbe4c0ceacbf9e7289b9e5145d9499ee
GNorfolk <georgenorfolk@hotmail.co.uk>
2017-10-10 12:16:19 +0100

    Added sign in view to application layout and presence validation.

--- showme_project/app/models/article.rb
+++ showme_project/app/models/article.rb
@@ -1,3 +1,14 @@
 class Article < ApplicationRecord
 	belongs_to :user
+
+	validates :title, :description, :bg_image, :proj_image, :git_link, :difficulty, :user_id presence: true
+
+    # t.string "title"
+    # t.text "description"
+    # t.string "bg_image"
+    # t.string "proj_image"
+    # t.string "git_link"
+    # t.integer "difficulty"
+    # t.integer "user_id"
+
 end
