a27545bd49aa52f2dca9e06d8702a86dc7a8a2e6
jack-a-scott <jack.scott@ntlworld.com>
2017-10-11 17:09:15 +0100

    updated controller and index page to allow users to promote admins

---
+++ showme_project/app/controllers/users_controller.rb
@@ -0,0 +1,29 @@
+class UsersController < ApplicationController
+
+
+  def index
+    @users = User.all
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+    @user = User.find(params[:id])
+    @user.admin = true
+    @user.save
+      redirect_to '/users'
+  end
+
+  def destroy
+  end
+end
--- showme_project/config/routes.rb
+++ showme_project/config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   
+  get 'users', to: 'users#index'
+
+  put 'users/:id', to: 'users#update'
+
   resources :articles
 
   root "static#home"
