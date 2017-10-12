4d219cf0b148e3cc6213dc8bf30d47b7122e416c
jack-a-scott <jack.scott@ntlworld.com>
2017-10-12 11:35:21 +0100

    improved comments and indentation on layouts page and controllers

--- showme_project/app/controllers/users_controller.rb
+++ showme_project/app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
 
-
+  # Takes all users and arranges into a table on the index
   def index
     @users = User.all
   end
@@ -8,15 +8,7 @@ class UsersController < ApplicationController
   def show
   end
 
-  def new
-  end
-
-  def create
-  end
-
-  def edit
-  end
-
+  # Updates a users role to being an admin
   def update
     @user = User.find(params[:id])
     @user.admin = true
@@ -24,6 +16,7 @@ class UsersController < ApplicationController
       redirect_to '/users'
   end
 
+  # Deletes a user account
   def destroy
     @user = User.find(params[:id])
     @user.delete
