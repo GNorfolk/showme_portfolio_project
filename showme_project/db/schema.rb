09d9199583535998d5d5e4a8b389ac21986ded54
Paras-C <paras_c@hotmail.co.uk>
2017-10-13 16:08:01 +0100

    Replacing image tags

--- showme_project/db/schema.rb
+++ showme_project/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171013134459) do
+ActiveRecord::Schema.define(version: 20171013145318) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,8 +18,6 @@ ActiveRecord::Schema.define(version: 20171013134459) do
   create_table "articles", force: :cascade do |t|
     t.text "title"
     t.text "description"
-    t.string "bg_image"
-    t.string "proj_image"
     t.string "git_link"
     t.integer "difficulty"
     t.integer "user_id"
