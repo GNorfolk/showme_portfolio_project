8dbb477e99b9273a0e5fbe670bc7bd5d60497b27
jack-a-scott <jack.scott@ntlworld.com>
2017-10-13 13:42:25 +0100

    merge

--- showme_project/db/schema.rb
+++ showme_project/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171013100718) do
+ActiveRecord::Schema.define(version: 20171013110039) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
